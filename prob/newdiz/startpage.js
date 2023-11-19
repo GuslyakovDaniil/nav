b9dae2eb137f83a6c15f3713d62614272813c393
DaniilGus <d.daniil@inbox.ru>
2023-11-19 19:41:24 +0300

    auto scrolling

--- prob/newdiz/startpage.js
+++ prob/newdiz/startpage.js
@@ -1,75 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const filterBtn = document.getElementById("filterBtn");
-    const filterMenu = document.querySelector(".filter-menu");
-
-    filterBtn.addEventListener("click", function () {
-        filterMenu.style.display = (filterMenu.style.display === "block") ? "none" : "block";
-    });
-
-    // Добавьте обработчики для каждой кнопки фильтра и их функционала
-
-    const seasonFilter = document.getElementById("seasonFilter");
-    const ageFilter = document.getElementById("ageFilter");
-    const ratingFilter = document.getElementById("ratingFilter");
-
-    seasonFilter.addEventListener("click", function () {
-        // Реализуйте логику для обработки клика по кнопке "Сезон"
-        console.log("Сезон");
-    });
-
-    ageFilter.addEventListener("click", function () {
-        // Реализуйте логику для обработки клика по кнопке "Возраст"
-        console.log("Возраст");
-    });
-
-    ratingFilter.addEventListener("click", function () {
-        // Реализуйте логику для обработки клика по кнопке "Оценка"
-        console.log("Оценка");
-    });
-});
-document.getElementById('filterBtn').addEventListener('click', function () {
-    var popup = document.getElementById('filterPopup');
-    if (popup.style.display === 'block') {
-        popup.style.display = 'none';
-    } else {
-        popup.style.display = 'block';
-    }
-});
-
-function toggleDropdown(dropdownId) {
-    var dropdown = document.getElementById(dropdownId);
-    if (dropdown.style.display === 'block') {
-        dropdown.style.display = 'none';
-    } else {
-        dropdown.style.display = 'block';
-    }
-}
-
-function selectCategory(labelElement) {
-    labelElement.classList.toggle('selected');
-}
-
-document.getElementById('seasonBtn').addEventListener('click', function () {
-    toggleDropdown('seasonOptions');
-});
-
-document.getElementById('genderBtn').addEventListener('click', function () {
-    toggleDropdown('genderOptions');
-});
-
-document.getElementById('ratingBtn').addEventListener('click', function () {
-    toggleDropdown('ratingOptions');
-});
-
-// Add event listeners for label clicks in each category
-document.querySelectorAll('.dropdown-content label').forEach(function (label) {
-    label.addEventListener('click', function () {
-        selectCategory(label);
-    });
-});
-
-
-// SVG карта===============================================================================================================
 function updateSVG() {
     const screenWidth = window.innerWidth;
     const screenHeight = window.innerHeight;
@@ -80,7 +8,7 @@ function updateSVG() {
     const svg = document.getElementById('my-svg');
 
     // Center the SVG within the viewport
-    const xOffset = -screenWidth / 4.5; // Adjust as needed
+    const xOffset = -screenWidth / 3.7; // Adjust as needed
     const yOffset = -screenHeight / 0.7 + addedHeight; // Adjust as needed
 
     // Set viewBox with the offset values
@@ -446,7 +374,7 @@ function showRoomInfo(roomNumber, popup, roomTexts) {
 
     // Устанавливаем позицию всплывающего окна
     popup.style.left = '0.4%';
-    popup.style.top = '25%';
+    popup.style.top = '150%';
 
     // Показываем всплывающее окно
     popup.style.display = 'block';
